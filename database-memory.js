678712ed3b3b1d09b554bc495301b8b7671099d0
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-09-10 17:48:08 -0300

    Database modificado com comentario

--- database-memory.js
+++ database-memory.js
@@ -1,6 +1,7 @@
 import { randomUUID } from 'node:crypto'
 
 export class DatabaseMemory {
+    //Map -> Modelo de dados usado para sempre requisitar -> (key, value)
     #videos = new Map()
 
     list() {
